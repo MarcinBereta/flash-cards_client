a01179aa021b32172aa01bbadb78326111550d68
Reckit075 <mateusz.majka.zsl@gmail.com>
2020-07-13 01:08:43 +0200

    login_page mechanika

--- src/views/login_page/index.js
+++ src/views/login_page/index.js
@@ -4,6 +4,7 @@ import Background from '../../components/background'
 import Form from '../../components/form/'
 import Input from '../../components/input/'
 import Button from '../../components/button/'
+import Spinner from 'react-bootstrap/Spinner'
 
 const Label = styled.label`
   display: inline-block;
@@ -24,18 +25,104 @@ margin: 0 auto;
 margin-top: 3vh;
 width: 90%;
 `
+const AlertDiv = styled.div`
+width:15vw;
+height:5vh;
+color:red;
+text-align:center;
+font-weight:bold;
+font-size:30px;
+`
 
-const LoginView = () =>{
+const LoginView = (props) =>{
+    const[state, setState] = useState({
+        username:"",
+        password:"",
+        spinner:false,
+    })
+
+    const [alert, setAlert] = useState("")
+
+    // const redirect = path =>{
+    //     props.history.push(path)
+    // }
+
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+
+        if(
+            state.username.trim() === ""
+            || state.password.trim() === ""
+        ){
+            setAlert("Please add all data!!!")
+            return
+        }
+
+
+        fetch("http://127.0.0.1:3001/users/login",{
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: state.username.trim(),
+                password: state.password.trim()
+            })
+        })
+            .then(res =>{
+                if(res.ok){
+                    return res.json()
+                }else{
+                    setAlert("Błąd serwera!")
+                }
+            })
+            .then(data =>{
+                if(!data.succes){
+                    setAlert(data.message)
+                }else{
+                    setAlert(data.message)
+                    setState({
+                        ...state,
+                        spinner:true
+                    })
+                    setTimeout(()=>{
+                        props.history.push('/')
+                    },3000)
+                }
+            })
+
+    }
+    const handleChange = event =>{
+        setState({
+            ...state,
+            [event.target.name]: event.target.value
+        })
+    }
     return(
         <Background width="100%">
-            <Form>
+            <Form onSubmit={handleSubmit}>
             <InputGroup>
-                <Label>name</Label><Input type="text" name="username" placeholder='name'/>
+                <Label>name</Label><Input type="text" name="username" onChange={handleChange} value={state.username} placeholder='name'/>
             </InputGroup>
             <InputGroup>
-                <Label>password</Label><Input type="password" name="password" placeholder='password'/>
+                <Label>password</Label><Input type="password" name="password" onChange={handleChange} value={state.password} placeholder='password'/>
             </InputGroup>
+            <Spinner animation="border" role="status">
+  <span className="sr-only">Loading...</span>
+</Spinner>
             <Button text={'Login'} />
+            {
+                    alert !== ""?
+                    <AlertDiv>
+                        {alert}
+                    </AlertDiv>
+                    :
+                    null
+                }
+            {
+                (state.spinner == true) && <Spinner animation="border" />
+            }
             </Form>
         </Background>
     )
